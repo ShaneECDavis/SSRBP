f1c52fb105b1817a41e2984e2ba4cd16e8c18a95
Shane EC Davis <shaneecdavis7@gmail.com>
2018-12-06 09:35:20 -0600

    base files written

---
+++ src/shared/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React in the Server</h2>
+        </div>
+        <p className="App-intro">SSR is neat </p>
+      </div>
+    )
+  }
+}
